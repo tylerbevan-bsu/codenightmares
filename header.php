66c23094bb6d033a33c648a9608ccaddf38251c2
Tyler Bevan <tylerbevan@boisestate.edu>
2018-01-31 22:14:39 -0700

    More bug fixes

--- header.php
+++ header.php
@@ -2,8 +2,9 @@
 <html>
 <head>
 <link href='/style.css' rel='stylesheet'>
-<div style='height=120px; width=100%'>
-<h1 style='float: left'>Code Nightmares</h1>
-<img src='/assets/logo.png' alt='Logo' style='float:right;height=100px;width=100px'>
-</div>
 </head>
+<body>
+<div style='height:120px; width:100%; position:absolute; top:0px'>
+<h1 style='float: left; line-height:120px'>Code Nightmares</h1>
+<img src='/assets/logo.png' alt='Logo' style='float:right;height:100px;width:100px'>
+</div>
