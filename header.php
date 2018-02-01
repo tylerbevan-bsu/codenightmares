e94c360e2b25c96db3e9425d7f0d6021931ab535
Tyler Bevan <tylerbevan@boisestate.edu>
2018-02-01 16:37:40 -0700

    Another Header fix attempt

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <link href='/style.css' rel='stylesheet'>
 </head>
 <body>
-<div style='height:120px; width:100%; position:absolute; top:0px'>
-<h1 style='float: left; line-height:120px'>Code Nightmares</h1>
-<img src='/assets/logo.png' alt='Logo' style='float:right;height:100px;width:100px'>
-</div>
+	<div style='height:120px; width:100%; position:absolute; top:0px'>
+		<h1 style='line-height:120px'>Code Nightmares</h1>
+		<img src='/assets/logo.png' alt='Logo' style='float:right;height:100px;width:100px'>
+	</div>
