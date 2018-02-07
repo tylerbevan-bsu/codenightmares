db644024df33131378c65adcb0fd88838ae25812
Tyler Bevan <tylerbevan@boisestate.edu>
2018-02-06 21:57:40 -0700

    Changed index.php to redirect to the popular.php page.

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
-	require_once('header.php');
-?>
-<!-- Content goes here  -->
-<p>
-This is a paragraph.
-</p>
 
-<!-- End content        -->
-<?php
-	require_once('footer.php');
+header( "Location: /popular.php" )
+
 ?>
