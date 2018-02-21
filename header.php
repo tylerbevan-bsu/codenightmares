2f21c774c87a2ad9431088e2024baf9a93f68eb7
Tyler Bevan <tylerbevan@boisestate.edu>
2018-02-21 12:39:06 -0700

    Add some quotes to prevent php7 deprecation warnings.

--- header.php
+++ header.php
@@ -10,10 +10,10 @@
 		<img src='/assets/logo.png' alt='Logo' class='title'>
 	</div>
 	<div class='navbar'>
-		<a class='navbar' href='popular.php' <?php if ($_SERVER[REQUEST_URI] == '/popular.php') echo "id='current-page'"; ?>>Popular</a>
-		<a class='navbar' href='latest.php' <?php if ($_SERVER[REQUEST_URI] == '/latest.php') echo "id='current-page'"; ?>>Latest</a>
-		<a class='navbar' href='top.php' <?php if ($_SERVER[REQUEST_URI] == '/top.php') echo "id='current-page'"; ?>>Top</a>
-		<a class='navbar' href='login.php' <?php if ($_SERVER[REQUEST_URI] == '/login.php') echo "id='current-page'"; ?>>Login</a>
+		<a class='navbar' href='popular.php' <?php if ($_SERVER['REQUEST_URI'] == '/popular.php') echo "id='current-page'"; ?>>Popular</a>
+		<a class='navbar' href='latest.php' <?php if ($_SERVER['REQUEST_URI'] == '/latest.php') echo "id='current-page'"; ?>>Latest</a>
+		<a class='navbar' href='top.php' <?php if ($_SERVER['REQUEST_URI'] == '/top.php') echo "id='current-page'"; ?>>Top</a>
+		<a class='navbar' href='login.php' <?php if ($_SERVER['REQUEST_URI'] == '/login.php') echo "id='current-page'"; ?>>Login</a>
 	</div>
 
 <main>
