5b7b75c1fec8797ab8268cde3b64fb40d84a23ef
Tyler Bevan <tylerbevan@boisestate.edu>
2018-02-21 12:37:51 -0700

    Added an about page.

---
+++ about.php
@@ -0,0 +1,16 @@
+<?php
+	require_once('header.php');
+?>
+<!-- Content goes here  -->
+
+<p>
+This site is for CS401 at Boise State University.
+</p>
+<p>
+It is powered by <a href="https://www.centos.org/">CentOS</a>, <a href="https://nginx.org/en/">NGINX</a>, <a href="https://mariadb.org/">MariaDB</a> (eventually), <a href="https://jquery.com/">jQuery</a> (eventually),and <a href="https://secure.php.net/">PHP7</a>. It is being hosted on <a href="https://cloud.google.com/products/compute/">Google Cloud</a>.
+</p>
+
+<!-- End content        -->
+<?php
+	require_once('footer.php');
+?>
--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
 </main>
 <footer>
 <h4 class='footleft'> Copyright Tyler Bevan, 2018 </h4>
+<a href="about.php"><h4 class='footcenter'> About </h4></a>
 <a href="https://php.net/"><h4 class='footright'> Powered by black magic </h4></a>
 </footer>
 </article>
