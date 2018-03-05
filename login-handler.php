4aa9a2836b2df1dd82c0d1f24e6d20fab728b993
Tyler Bevan <tylerbevan@boisestate.edu>
2018-03-05 16:27:32 -0700

    Debugging code for login.

--- login-handler.php
+++ login-handler.php
@@ -1,8 +1,14 @@
 <?php
-  #require_once '../Dao.php';
-  #$dao = new Dao();
-  #$name = $_POST["name"];
-  #$comment = $_POST["comment"];
-  #$dao->saveComment($name, $comment);
-  header("Location: login.php");
+  require_once '../Dao.php';
+  $dao = new Dao();
+  $name = $_POST["username"];
+  $comment = $_POST["password"];
+  if ($dao->checkUsernameExists($username)) {
+    echo "Username Does Not Exist" ;
+  } elseif ($dao->checkPassword($username, $password)) {
+    echo "Incorrect Password" ;
+  } else {
+    echo "Login Success" ;
+  }
+  ##header("Location: login.php");
   exit;
